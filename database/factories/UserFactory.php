0900ff576612f5c1b72863ee8a33720800106384
ommer1321 <1453o.faruk@gmail.com>
2022-12-23 14:12:18 +0300

    video-3

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,24 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+
+
+        User::insert([
+
+            'name'=>'Ömer Faruk Çetin',
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'type'=>'admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]); 
+        
+        $types=['admin','user'];
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'type'=>$types[rand(0,1)],
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,18 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/panel', function () {
         return view('dashboard');
     })->name('dashboard');
+
+ 
+});
+
+
+Route::group(['middleware'=>['auth','isAdmin'],'prefix'=>'admin'],function(){
+
+    Route::get('deneme',function(){
+        return " Testi";
+        
+    });
 });
