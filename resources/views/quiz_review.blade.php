5e719b9aac7a66af4c0cea535e1ebdd9e8c0a025
ommer1321 <1453o.faruk@gmail.com>
2022-12-30 16:26:35 +0300

    video 23 devamı

--- resources/views/quiz_review.blade.php
+++ resources/views/quiz_review.blade.php
@@ -31,13 +31,13 @@
       <div class="form-check">
         <input  class="form-check-input" type="radio" @if ($question->myAnswer->answer == 'answer1'  ) checked   @else disabled  @endif  value="answer1" >
         <label class="form-check-label" for="exampleRadios1">
-          1- @if ($question->correct_answer == 'answer1'   )<b><span style="color: rgb(0, 244, 0)"> {{$question->answer1}}  </b> </span>  @else  {{$question->answer1}}     @endif
+         @if ($question->correct_answer == 'answer1'   )<b><span style="color: rgb(0, 244, 0)">  1- {{$question->answer1}}  </b> </span>  @else 1- {{$question->answer1}}    @endif
       </div>
     
       <div class="form-check">
         <input  class="form-check-input" type="radio" @if ($question->myAnswer->answer == 'answer2'  ) checked   @else disabled  @endif  value="answer2" >
         <label class="form-check-label" for="exampleRadios1">
-          2- @if ($question->correct_answer == 'answer2'   ) <b><span style="color: green"> {{$question->answer2}} </b>   </span>  @else  {{$question->answer2}}     @endif
+          @if ($question->correct_answer == 'answer2'   ) <b><span style="color: green">  2- {{$question->answer2}} </b>   </span>  @else 2- {{$question->answer2}}    @endif
         </label>
       </div>
       
@@ -45,14 +45,14 @@
       <div class="form-check">
         <input  class="form-check-input" type="radio" @if ($question->myAnswer->answer == 'answer3'  ) checked   @else disabled  @endif    value="answer3" >
         <label class="form-check-label" for="exampleRadios1">
-          3- @if ($question->correct_answer == 'answer3'   )<b><span style="color: rgb(0, 244, 0)"> {{$question->answer3}}  </b> </span>  @else  {{$question->answer3}}     @endif
+           @if ($question->correct_answer == 'answer3'   )<b><span style="color: rgb(0, 244, 0)"> 3- {{$question->answer3}}  </b> </span>  @else 3- {{$question->answer3}}     @endif
         </label>
       </div>
     
       <div class="form-check">
         <input  class="form-check-input" type="radio" @if ($question->myAnswer->answer == 'answer4'  ) checked   @else disabled  @endif     value="answer4" >
         <label class="form-check-label" for="exampleRadios1">
-          4- @if ($question->correct_answer == 'answer4'   )<b><span style="color: rgb(0, 244, 0)"> {{$question->answer4}}  </b> </span>  @else  {{$question->answer4}}     @endif
+         @if ($question->correct_answer == 'answer4'   )<b><span style="color: rgb(0, 244, 0)">4- {{$question->answer4}}  </b> </span>  @else  4- {{$question->answer4}}    @endif
         </label>
       </div>
     
